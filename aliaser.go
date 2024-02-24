5032029e65eeeb23410ee7eefa4738efaa0d0164
Bradley Caliva <bcaliva21@gmail.com>
2024-02-24 10:38:49 -0600

    style(aliaser): format

--- aliaser.go
+++ aliaser.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 var (
@@ -28,11 +28,11 @@ func main() {
 
 	if help {
 		fmt.Println("Usage: aliaser [options]")
-        fmt.Println("Options:")
-        fmt.Println("  -p, --path <path>      Path to assign alias to")
-        fmt.Println("  -a, --alias <alias>  Alias to assign to path")
-        fmt.Println("  -h, --help           Show help message and exit")
-        os.Exit(0)
+		fmt.Println("Options:")
+		fmt.Println("  -p, --path <path>      Path to assign alias to")
+		fmt.Println("  -a, --alias <alias>  Alias to assign to path")
+		fmt.Println("  -h, --help           Show help message and exit")
+		os.Exit(0)
 	}
 
 	if path == "" || alias == "" {
@@ -40,24 +40,24 @@ func main() {
 		os.Exit(1)
 	}
 
-    // Append config to .zshrc
-    zshrcPath := fmt.Sprintf("%s/.zshrc", os.Getenv("HOME"))
-    contents, err := ioutil.ReadFile(zshrcPath)
-    if err != nil {
-        fmt.Println("Error reading .zshrc:", err)
-        os.Exit(1)
-    }
+	// Append config to .zshrc
+	zshrcPath := fmt.Sprintf("%s/.zshrc", os.Getenv("HOME"))
+	contents, err := ioutil.ReadFile(zshrcPath)
+	if err != nil {
+		fmt.Println("Error reading .zshrc:", err)
+		os.Exit(1)
+	}
 
-    newContents := fmt.Sprintf(`alias %s=%s`, alias, path)
-    if !strings.Contains(string(contents), newContents) {
-        contents = append(contents, []byte(newContents+"\n")...)
-        err = ioutil.WriteFile(zshrcPath, contents, 0644)
-        if err != nil {
-            fmt.Println("Error writing to .zshrc:", err)
-            os.Exit(1)
-        }
-    }
+	newContents := fmt.Sprintf(`alias %s=%s`, alias, path)
+	if !strings.Contains(string(contents), newContents) {
+		contents = append(contents, []byte(newContents+"\n")...)
+		err = ioutil.WriteFile(zshrcPath, contents, 0644)
+		if err != nil {
+			fmt.Println("Error writing to .zshrc:", err)
+			os.Exit(1)
+		}
+	}
 
-    fmt.Println("Alias created! You can now use", alias, "to access", path)
+	fmt.Println("Alias created! You can now use", alias, "to access", path)
 }
 
